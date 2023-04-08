f3ad84ceaec070be6d165b5758022b2de480bdb3
algoksxun <130209866+algoksxun@users.noreply.github.com>
2023-04-08 12:43:11 +0500

    Update mef.py

--- mef.py
+++ mef.py
@@ -1,45 +1,32 @@
-print('helo mir
-wwww
-import pageme
-in put"privet"
+print("helo mir")
+
+import pygeme
+input("privet")
 
 a = input("preven")
 while True:
-      print('''w''''''ww'ww')
-      '''inpat("hahahahahaha")
-      print(a, 'ti dahyhn)'
-            ;lkjhg
-      break
-
-            pygame =standof
+      print('wwwww')
+      input("hahahahahaha")
+      print(a, "ti dahyhn")
+      break 
             
-            wwwwwwww
-            awq2
-            
-pygame.enot()
-            while tru:
-            print(;sssswqimjunh7bg6f56y78912{'{@}}}
+pygame.init():
+            while True:
+            print("sssswqimjunh7bg6f56y78912{'{@}}}")
             break
-     '
+     #'
                                              
-                                             w
-                                3
-       2
-                                            5
-                                                                7
-                       8
-                                            9
-             2\
-                                             3
-                                     3
-                         7
-                                                        9
-PAGEME.QUERT><LKJUY(*))
-                               aawawaaw
-                                             awawwaaw
-                                             w
-                                             aw
-                                             aw
-                                             aw
-                                             aw
-                                                             w
+                                             #w
+                                #3
+       #2
+                                            #5
+                                                                #7
+                       #8
+                                           # 9
+             #2\
+                                             #3
+                                     #3
+                         #7
+                                                       #9
+pygame.QUIT()
+                               
