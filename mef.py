a43982b9e941014fdf96a4f01fef367456dcea5b
ЦУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУССССССССССССССССССССССССССССССССССССССССКККККККККККККККККККУУУ <130209894+qwe123qwe123qwe123qwe123qwe123@users.noreply.github.com>
2023-04-08 12:32:47 +0500

    Create mef.py

---
+++ mef.py
@@ -0,0 +1,45 @@
+print('helo mir
+wwww
+import pageme
+in put"privet"
+
+a = input("preven")
+while True:
+      print('''w''''''ww'ww')
+      '''inpat("hahahahahaha")
+      print(a, 'ti dahyhn)'
+            ;lkjhg
+      break
+
+            pygame =standof
+            
+            wwwwwwww
+            awq2
+            
+pygame.enot()
+            while tru:
+            print(;sssswqimjunh7bg6f56y78912{'{@}}}
+            break
+     '
+                                             
+                                             w
+                                3
+       2
+                                            5
+                                                                7
+                       8
+                                            9
+             2\
+                                             3
+                                     3
+                         7
+                                                        9
+PAGEME.QUERT><LKJUY(*))
+                               aawawaaw
+                                             awawwaaw
+                                             w
+                                             aw
+                                             aw
+                                             aw
+                                             aw
+                                                             w
